80713f47760959ba23c0bc1d9876ad5c98969d2d
andriy02k <krainyk02@gmail.com>
2024-04-18 03:32:22 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const Header = lazy(() => import('./Header/Header'));
-const Home = lazy(() => import('../pages/Home/Home'));
-const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
-const Favorites = lazy(() => import('../pages/Favorites/Favorites'));
+const Home = lazy(() => import('../Pages/Home/Home'));
+const Catalog = lazy(() => import('../Pages/Catalog/Catalog'));
+const Favorites = lazy(() => import('../Pages/Favorites/Favorites'));
 
 const App = () => {
   return (
